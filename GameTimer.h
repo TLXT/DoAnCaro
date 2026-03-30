9a2ee2c899e92e97de860e19eac6f65598535811
Nguyễn Gia Hưng <giahungandfriends@gmail.com>
2026-03-30 07:50:03 +0700

    add gametimer

---
+++ GameTimer.h
@@ -0,0 +1,21 @@
+﻿#ifndef _GAMETIMER_H_
+#define _GAMETIMER_H_
+
+#include <atomic> // Đảm bảo việc tính và ghi thời gian không bị ảnh hưởng khi 2 luồng cùng tính
+#include <thread>
+#include <mutex>
+#include <iostream>
+using namespace std;
+
+const int TURN_TIME_LIMIT = 30;
+
+extern atomic<int> timeLeft;
+extern atomic<bool> isPaused;
+extern atomic<bool> isTimerRunning;
+
+// Các hàm xử lý
+void StartTimerThread();
+void StopTimerThread();
+void TimerLogic(); // Hàm chạy trong luồng phụ
+
+#endif
